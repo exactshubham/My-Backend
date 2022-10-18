1240f6b85d66bfd8010139a9278a847751d0e913
exactshubham <exactshubham@gmail.com>
2022-10-18 14:35:14 +0530

    This is the assignment of Path params

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const { arrayBuffer } = require('stream/consumers');
 const abc = require('../introduction/intro')
 const router = express.Router();
+
+
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
@@ -12,7 +15,82 @@ router.get('/students', function (req, res){
     console.log("The path params in the request are : ", req.params)
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
+});
+
+router.get('/movies', function (req, res){                 //Done by me
+    console.log("These are movies : ", req.params)                                  
+    let movies = ['Rang de basanti','The shining','Lord of the rings', 'Batman begins'] 
+    res.send(movies)
+});
+
+router.get('/movies/:indexNumber',function(req, res){
+    let movies = ['Rang de basanti','The shining','Lord of the rings', 'Batman begins']
+    let indexNumber = req.params.indexNumber
+    if(indexNumber>movies.length||indexNumber<0){
+        res.send("please give me valid index number")
+    } 
+    res.send(movies[indexNumber])
 })
+
+router.get('/films',function(req, res){
+  let films =  [{
+        "id" : 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+       res.send(films)
+    })    
+
+router.get('/films/:filmId',function(req, res){
+    let films =  [{
+        "id" : 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+      
+        
+      
+
+git 
+    let b =req.params.filmId
+    let c =films[b-1]
+    if(films.length>=b){
+        res.send(c)
+     }else{
+        res.send("NO movie exist with this Id")
+    }
+   
+})
+
+// //const id =3    //req.params
+
+// const movie =moviesArray.find(film => film.id==id)
+
+// if(movie){
+//     res.send(movie)
+// }else{
+//     res.send("no movies exist with this id")
+// }
+
+
+
+
 
 
 // Example 1 for path params
