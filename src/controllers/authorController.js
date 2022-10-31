e187b12594b46f21be63e5497f55bd981cbad574
exactshubham <115546713+exactshubham@users.noreply.github.com>
2022-10-31 21:24:08 +0530

    Update authorController.js

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -3,6 +3,9 @@ const AuthorModel= require("../models/authorModel")
 const createAuthor= async function (req, res) {
     let author = req.body
     let authorCreated = await AuthorModel.create(author)
+    //console.log("this line will execute before the above line") due to async behaviour which we made by using async,await
+    // Javascript is Synchronous,Single Threaded with Blocking Model.
+    // By async, await we made it Asynchronous, Multi Threaded, Non Blocking Model.
     res.send({data: authorCreated})
 }
 
