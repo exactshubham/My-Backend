e9938a8ff52b90baf69ad3523043b0d5bade43be
exactshubham <exactshubham@gmail.com>
2022-11-03 23:40:31 +0530

    earlystart

---
+++ src/controllers/assUserController.js
@@ -0,0 +1,72 @@
+const jwt = require("jsonwebtoken");
+const assUserModel = require("../models/assUserModel");
+
+
+const createUSER = async function (abcd, xyz) {
+ 
+  let data = abcd.body;
+  let savedData = await assUserModel.create(data);
+  console.log(abcd.newAtribute);
+  xyz.send({ msg: savedData });
+};
+
+const loginUSER = async function (req, res) {
+  let emailId = req.body.emailId;
+  let password = req.body.password;
+
+  let user = await assUserModel.findOne({ emailId: emailId, password: password });
+  if (!user) return res.send({ status: false, msg: "username or the password is not correct" })
+
+  let token = jwt.sign(
+    {
+      userId: user._id.toString(),
+      batch: "Lithium",
+      organisation: "FunctionUp",
+    },
+    "functionup-Lithium-very-very-secret-key"
+  );
+  res.setHeader("x-auth-token", token);
+  res.send({ status: true, token: token });
+};
+
+const getUSERData = async function (req, res) {
+  let token = req.headers["x-Auth-token"];
+  if (!token) token = req.headers["x-auth-token"];
+
+ 
+  if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  console.log(token);
+
+ 
+  let decodedToken = jwt.verify(token, "functionup-Lithium-very-very-secret-key");
+  if (!decodedToken)
+    return res.send({ status: false, msg: "token is invalid" });
+
+  let userId = req.params.userId;
+  let userDetails = await assUserModel.findById(userId);
+  if (!userDetails)
+    return res.send({ status: false, msg: "No such user exists" });
+
+  res.send({ status: true, data: userDetails });
+ 
+};
+
+const updateUSER = async function (req, res) {
+  
+  let userId = req.params.userId;
+  let user = await userModel.findById(userId);
+  
+  if (!user) {
+    return res.send("No such user exists");
+  }
+
+  let userData = req.body;
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
+  res.send({ status: updatedUser, data: updatedUser });
+};
+
+module.exports.createUSER = createUSER;
+module.exports.getUSERData = getUSERData;
+module.exports.updateUSER = updateUSER;
+module.exports.loginUSER = loginUSER;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const assUserController= require('../controllers/assUserController')
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/users", userController.createUser  )
+//router.post("/users", userController.createUser  )
 
-router.post("/login", userController.loginUser)
+//router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
+//router.get("/users/:userId", userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
+//router.put("/users/:userId", userController.updateUser)
 
+
+
+router.post("/USERS",assUserController.createUSER)
+router.post("/LOGIN",assUserController.loginUSER)
+router.get("/users/:userId", assUserController.getUSERData)
 module.exports = router;
