c1ed556b0eddf3b6792a60df211145d3be19f856
exactshubham <exactshubham@gmail.com>
2022-11-08 01:54:25 +0530

    done also with try and catch and used many status codes

--- src/controllers/assuserController.js
+++ src/controllers/assuserController.js
@@ -2,11 +2,15 @@ const jwt= require("jsonwebtoken");
 const assuserModel= require("../models/assuserModel")
 
 const createUser = async function (abcd, xyz) {
-    let data = abcd.body;
+  try{ 
+  let data = abcd.body;
     let savedData = await assuserModel.create(data);
     console.log(abcd.newAtribute);
-    xyz.send({ msg: savedData });
-  };
+    xyz.status(201).send({ msg: savedData });
+  }catch(error){
+     xyz.status(500).send({status: false, message : error.message })
+  }
+};
   
   const loginUser = async function (req, res) {
     let userName = req.body.emailId;    //let {username,password} = req.body
@@ -14,9 +18,9 @@ const createUser = async function (abcd, xyz) {
   
     let user = await assuserModel.findOne({ emailId: userName, password: password });
     if (!user)
-      return res.send({
+      return res.status(400).send({
         status: false,
-        msg: "username or the password is not corerct",
+        msg: "username or the password is not correct",
       });
   
     let token = jwt.sign(
@@ -28,64 +32,97 @@ const createUser = async function (abcd, xyz) {
       "functionup-Lithium"
     );
     res.setHeader("x-auth-token", token);
-    res.send({ status: true, data: token });
+    res.status(200).send({ status: true, data: token });
   };
   
   const getUserData = async function (req, res) {
+    try{
     let token = req.headers["x-Auth-token"];
     if (!token) token = req.headers["x-auth-token"];
   
-    if (!token) return res.send({ status: false, msg: "token must be present" });
+    if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
   
     console.log(token);
    
     let userId = req.params.userId;
-    let userDetails = await assuserModel.findById(userId);
-    if (!userDetails)
-      return res.send({ status: false, msg: "No such user exists" });
-  
-    res.send({ status: true, data: userDetails });
-  };
+     decodedToken=req.user;
+
+    if(userId===decodedToken.userId){
+
+    let userDetails = await assuserModel.findOne({_id:userId});
+    if (userDetails){
+    res.status(200).send({ status: true, data: userDetails });
+    }else{
+    return res.status(404).send({ status: false, msg: "No such user exists" })};
+
+  }else{
+    res.status(403).send({status: false, message: "Prohibited as maybe trying to accesss a different user's account"})
+  }
+}catch(error){
+   res.status(500).send({status: false, error: error.message})
+ }}
   
   const updateUser = async function (req, res) {
-    let token = req.headers["x-Auth-token"];
-    if (!token) token = req.headers["x-auth-token"];
-  
-    if (!token) return res.send({ status: false, msg: "token must be present" });
-   
-  
-    let userId = req.params.userId;
-    let user = await assuserModel.findById(userId);
-  
-    if (!user) {
-      return res.send("No such user exists");
-    }
-  
+    try{
+      let token = req.headers["x-Auth-token"];
+      if (!token) token = req.headers["x-auth-token"];
+    
+      if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
+    
+      console.log(token);
+     
+      let userId = req.params.userId;
+      let decodedToken=req.user;
+
+      if(userId===decodedToken.userId){
+
+      let userDetails = await assuserModel.findOne({_id:userId});
+      if (userDetails){
+      res.status(200).send({ status: true, data: userDetails });
+      }else{
+      return res.send({ status: false, msg: "No such user exists" })};
+
+    }else{
+      res.status(403).send({status: false, message: "Prohibited as maybe trying to accesss a different user's account"})};
+
     let userData = req.body;
     let updatedUser = await assuserModel.findOneAndUpdate({ _id: userId }, userData);
     res.send({ status: true, data: updatedUser, new :true });
-  };
+
+  }catch(error){
+    res.status(500).send({status: false, error: error.message})
+  }}
 
   const deleteUser = async function(req, res){
-     let token = req.headers["x-Auth-token"];
-    if (!token) token = req.headers["x-auth-token"];
-  
-    if (!token) return res.send({ status: false, msg: "token must be present" });
-      
-    let userId =req.params.userId;
-    let user = await assuserModel.findById(userId);
+    try{
+      let token = req.headers["x-Auth-token"];
+      if (!token) token = req.headers["x-auth-token"];
+    
+      if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
+    
+      console.log(token);
+     
+      let userId = req.params.userId;
+      let decodedToken=req.user;
 
-    if (!user) {
-        return res.send("No such user exists");
-      }
-      
-   
-    let updatedUser = await assuserModel.findOneAndUpdate({ _id: userId }, {$set :{isDeleted: true}});
+      if(userId===decodedToken.userId){
+
+      let userDetails = await assuserModel.findOne({_id:userId});
+      if (userDetails){
+      res.status(200).send({ status: true, data: userDetails });
+      }else{
+      return res.send({ status: false, msg: "No such user exists" })};
+
+    }else{
+      res.status(403).send({status: false, message: "Prohibited as maybe trying to accesss a different user's account"})}
+  
+      let updatedUser = await assuserModel.findOneAndUpdate({ _id: userId }, {$set :{isDeleted: true}});
     res.send({ status: true , data: updatedUser, new :true });
+
+   }catch(error){
+    res.status(500).send({status: false, error: error.message})
+  }}
   
-  }
-  
- 
   
   module.exports.createUser = createUser;
   module.exports.loginUser = loginUser;
--- src/middleware/assauth.js
+++ src/middleware/assauth.js
@@ -3,36 +3,49 @@ const assuserModel=require("../models/assuserModel")
 
 
 const authenticate = async function(req, res, next) {
-   try{ let userId = req.params.userId;
-    let user = await assuserModel.findById(userId);
+   try{ let userId = req.params.userId;                                     
+    let user = await assuserModel.findById(userId);          
     if(!userId) {
       return res.status(400).send("userId is Required")
     };
 
     if (!user) {
-        return res.send("No such user exists")
+        return res.status(404).send("No such user exists")
       };
 
       let token = req.headers["x-auth-token"];
       if (!token) token = req.headers["x-Auth-token"];
     
-      if (!token) return res.send({ status: false, msg: "token must be present" });
-     
-    let decodedToken = jwt.verify(token, "functionup-Lithium",function(error){
-      if(error){
-        res.status(400).send({status: false, msg : "token is invalid"})
+      if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
+     else{
+    let decodedToken = jwt.verify(token, "functionup-Lithium")
+         if(decodedToken){
+          req.user=decodedToken
+          next();
       }else{
-       next() 
+        res.status(401).send({status: false, msg : "The authentication token is invalid"})
+      
       }
-    });
+    }
+    
+}catch(error){
+  return res.status(500).send({status:false, message : error.message})  // || "Invalid auth Token"
+}}
     // if (!decodedToken)
     //   return res.send({ status: false, msg: "token is invalid" });
     //check the token in request header
     //validate this token
 
-    
-}catch(error){
-  return res.status(401).send({status:false, message : error.message || "Invalid auth Token"})
-}}
+    // let decodedToken = jwt.verify(token, "functionup-Lithium",function(error){
+    // if(error){
+    //   res.status(401).send({status: false, msg : "token is invalid"})
+
+
+
+
 
 module.exports.authenticate=authenticate
+
+
+
+
